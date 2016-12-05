c5c4112b79bade781aab7b1ab014a2db20ff4843
Raphael Cohn <raphael.cohn@stormmq.com>
2016-12-05 10:05:15 +0000

    Initial commit

---
+++ ltmain.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# This file is part of libertine linux's package libtool. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/libertine-linux-packages/libtool/master/COPYRIGHT. No part of libertine linux's package libtool, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+# Copyright © 2016 The developers of libertine linux's package libtool. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/libertine-linux-packages/libtool/master/COPYRIGHT.
+
+
+set -e
+set -u
+set -f
+
+printf '%s\n' "Do not use libtool; use slibtool" 1>&2
+exit 1
